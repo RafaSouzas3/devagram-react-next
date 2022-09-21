3d6fedb6bfdaf2fc02ac8417077c30acf333a528
RafaSouzas3 <rafael_silveira1996@hotmail.com>
2022-09-21 12:28:22 -0300

    upload de imagens

---
+++ componentes/uploadImagem/index.js
@@ -0,0 +1,56 @@
+import { useRef } from "react";
+
+export function UploadImagem({
+    className ='',
+    setImagem,
+    imagemPreview,
+    imagemPreviewClassName = ''
+}) {
+    const referenciaInput = useRef (null);
+
+    const abrirSeletorArquivos = () => {
+        referenciaInput?.current?.click();
+    }
+
+    const aoAlterarImagem =() =>{
+
+        if (!referenciaInput?.current?.files?.lenght){
+            return;
+        }
+
+        const arquivo = referenciaInput?.current?.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(arquivo);
+        fileReader.onloadend = () =>{ 
+            setImagem({
+                preview: fileReader.result,
+                arquivo
+            })
+        }
+    }
+
+    return(
+        <div className= {`uploadImagemContainer ${className}`} onClick = {abrirSeletorArquivos}>
+            <button> Abrir seletor de arquivos </button>
+                {imagemPreview && (
+                    <div className="imagemPreviewContainer">
+                        <img 
+                            src= {imagemPreview}
+                            alt='Imagem preview'
+                            className={imagemPreviewClassName}
+                        />
+
+                    </div>
+                )}
+
+            <input 
+                type = 'file'
+                className = 'oculto' 
+                accept ="image/*"
+                ref = {referenciaInput}
+                onChange ={aoAlterarImagem}
+                />
+                
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
+import Avatar from '../componentes/avatar'
+import Botao from '../componentes/botao'
+import { UploadImagem } from '../componentes/uploadImagem'
+
 
 
 export default function Home() {
+  const [imagem , setImagem] = useState(null);
+  console.log(imagem);
+
   return (
-        <h1 >
-          Olá Mundo!
-        </h1>
+    <>
+        <h1 >Olá Mundo!</h1>
+        <UploadImagem setImagem={setImagem} imagemPreview= {imagem?.preview}/>
+        <div style={{width : 200}}>
+          <Avatar/>
+          <Botao texto ={'Login'} cor= 'invertido' manipularClique={ () => console.log('botao clicado')}/>
+        </div>
+        </>
 
        
   )
